6d605c0d7d4694cb7b2c10cc8a663ff750d83c73
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-05-03 11:34:03 -0400

    gg

---
+++ SchoolStuff/src/practicefrq/Sign.java
@@ -0,0 +1,35 @@
+package practicefrq;
+
+import java.util.*;
+
+/*
+ 05-03-2024
+ augustjones
+ :3
+ */
+public class Sign {
+	private String str;
+	private int x;
+
+	public Sign(String str, int x) {
+		this.str = str;
+		this.x = x;
+	}
+
+	public String getSTR() {
+		return str;
+	}
+
+	public void setSTR(String str) {
+		this.str = str;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+}
