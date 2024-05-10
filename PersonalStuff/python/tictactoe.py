f85e8a9379db111955e461a4e80816685ae74c95
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-05-10 11:45:42 -0400

    gg

--- PersonalStuff/python/tictactoe.py
+++ PersonalStuff/python/tictactoe.py
@@ -1,5 +1,6 @@
 b = [["", "", ""], ["", "", ""], ["", "", ""]]
-turn: int = 0
+turn = 0
+
 
 def printBoard():
     print("    A   B   C  ")
@@ -8,14 +9,19 @@ def printBoard():
         for h in range(0, 3, 3):
             print(f"{w + 1} | {b[w][h]} | {b[w][h + 1]} | {b[w][h + 2]} |")
     print("  +---+---+---+")
-    
+
+
 def createBoard():
     for w in range(3):
-        for h in range(0,3,3):
+        for h in range(0, 3, 3):
             b[w][h] = " "
+
+
 def Drop():
-	player = ' '
- 	if turn == 0:
-    	player = 'X'
- 	elif turn == 1:
-    	player = 'O'
+    player = " "
+    if turn == 0:
+        player = "X"
+    elif turn == 1:
+        player = "O"
+    uI = input("Where do you want to place your "+ player+"?: ")
+    
