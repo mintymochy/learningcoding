3c02cc84296ef61583482b0e68e81fd9af7e69eb
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-05-16 12:11:49 -0400

    gg

--- C++/wordle/wordle.cpp
+++ C++/wordle/wordle.cpp
@@ -14,18 +14,18 @@ public:
     char board[5][6];
 
     void printGame() {
-        for (int r = 0; r < sizeOf(board) / sizeOf(int); r++) {
-            for (int w = 0; w < sizeOf(board) / sizeOf(int); w++) {
+        for (int r = 0; r < sizeof(board) / sizeof(int); r++) {
+            for (int w = 0; w < sizeof(board) / sizeof(int); w++) {
                 cout << board[r][w];
             }
             cout << "\n";
         }
     }
 
-    void createGame()) {
-        for (int r = 0; r < sizeOf(board) / sizeOf(int); r++) {
-            for (int w = 0; w < sizeOf(board) / sizeOf(int); w++) {
-                board[r][w] = " ";
+    void createGame() {
+        for (int r = 0; r < sizeof(board) / sizeof(int); r++) {
+            for (int w = 0; w < sizeof(board) / sizeof(int); w++) {
+                board[r][w] = ' ';
             }
         }
     }
