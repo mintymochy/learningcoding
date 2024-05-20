a8ba23205fd8e6b90d35baa646f01a318e9b3f81
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-05-20 18:06:32 -0400

    gg

--- C++/wordle/wordle.cpp
+++ C++/wordle/wordle.cpp
@@ -8,7 +8,7 @@ int main() {
 class wordle {
 public:
     vector<char> alp[26];
-    char board[5][6];
+    char board[6][5];
 
     void createALP(vector<char> arr1) {
         for (int x : arr1) {
@@ -17,9 +17,10 @@ public:
     }
 
     void printGame() {
+        cout << "------------------";
         for (int r = 0; r < sizeof(board) / sizeof(int); r++) {
-            for (int w = 0; w < sizeof(board) / sizeof(int); w++) {
-                cout << board[r][w];
+            for (int w = 0; w < sizeof(board[0]) / sizeof(int); w++) {
+                cout << "| " + board[r][w] + " | " + board[r][w + 1] + " | " + board[r][w + 2] + " | " + board[r][w + 3] + " | " + board[r][w + 4] + " | " + board[r][w + 5] + " | ";
             }
             cout << "\n";
         }
@@ -27,7 +28,7 @@ public:
 
     void createGame() {
         for (int r = 0; r < sizeof(board) / sizeof(int); r++) {
-            for (int w = 0; w < sizeof(board) / sizeof(int); w++) {
+            for (int w = 0; w < sizeof(board[0]) / sizeof(int); w++) {
                 board[r][w] = ' ';
             }
         }
