c5262b24565c2cce0af16f3748597e9a1a07ed56
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-05-31 11:46:08 -0400

    gg

---
+++ java/src/personal/battleshipsss/board.java
@@ -0,0 +1,17 @@
+package personal.battleshipsss;
+
+import java.util.*;
+
+/*
+ 05-31-2024
+ augustjones
+ :3
+ */
+public class board {
+    ArrayList<rows> board = new ArrayList<rows>();
+
+    public board() {
+        this.board = board;
+    }
+
+}
