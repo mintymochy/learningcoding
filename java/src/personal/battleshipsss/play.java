bd448d47946bd84f02da5f25bc1ce2a6cd5e047f
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-06-05 13:21:54 -0400

    gg

--- java/src/personal/battleshipsss/play.java
+++ java/src/personal/battleshipsss/play.java
@@ -9,7 +9,8 @@ class play {
     static Scanner uI = new Scanner(System.in);
 
     public void playerTurn() {
-
+        System.out.print("what do you want to do next?: ");
+        String input = uI.nextLine();
     }
 
 }
