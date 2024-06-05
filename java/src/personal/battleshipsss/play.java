fa7c908cde6bccc7ff55c1602efdaa011090cdb9
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-06-05 13:04:01 -0400

    gg

---
+++ java/src/personal/battleshipsss/play.java
@@ -0,0 +1,15 @@
+package personal.battleshipsss;
+
+import java.util.*;
+
+/*
+ * :3
+ */
+class play {
+    static Scanner uI = new Scanner(System.in);
+
+    public void playerTurn() {
+
+    }
+
+}
