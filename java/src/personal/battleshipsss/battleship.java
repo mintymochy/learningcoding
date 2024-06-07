0b82a6822ef0a678c9fd5abcf9241da210c33072
Mocha <124919681+mintymochy@users.noreply.github.com>
2024-06-07 09:27:51 -0400

    gg

--- java/src/personal/battleshipsss/battleship.java
+++ java/src/personal/battleshipsss/battleship.java
@@ -22,7 +22,9 @@ class battleship {
     }
 
     public void printBoard() {
+        for (int x = 0; x < b.getBoard().size(); x++) {
 
+        }
     }
 
     public void attack() {
@@ -30,37 +32,38 @@ class battleship {
     }
 
     public void checkShot(String shot) {
+        int row;
         String letter = shot.substring(0, 1).toLowerCase();
         switch (letter) {
             case "a":
-
+                row = 0;
                 break;
             case "b":
-
+                row = 1;
                 break;
             case "c":
-
+                row = 2;
                 break;
             case "d":
-
+                row = 3;
                 break;
             case "e":
-
+                row = 4;
                 break;
             case "f":
-
+                row = 5;
                 break;
             case "g":
-
+                row = 6;
                 break;
             case "h":
-
+                row = 7;
                 break;
             case "i":
-
+                row = 8;
                 break;
             case "j":
-
+                row = 9;
                 break;
             default:
                 System.out.println("Error :3");
